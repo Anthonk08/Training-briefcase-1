2a88f74ae7350e6caf6633216228f9bb16332f34
Anthonk08 <anthonytineocabreja3@gmail.com>
2021-05-24 03:50:31 -0400

    Exercise #6 finished

--- Basic training/Javascript ES6/Practice/Exercises #1/6_Armstrong_numbers.js
+++ Basic training/Javascript ES6/Practice/Exercises #1/6_Armstrong_numbers.js
@@ -3,3 +3,21 @@ Armstrong Numbers.
 An Armstrong number is an n-digit number that is equal to the sum of the nthnth powers of its digits.
 Determine if the input number is an Armstrong number. Return either true or false.
 */
+
+const armstrongNumber = number => {
+    if(number <= 100 || number > 999){
+        return false;
+    }
+    let numStr = String(number);
+    let sum = 0;
+    numStr.split('').forEach(num => {
+        numInt = parseInt(num);
+        sum += Math.pow(numInt, 3);
+    });
+    return number == sum;
+}
+
+console.log(armstrongNumber(371));
+console.log(armstrongNumber(584));
+console.log(armstrongNumber(153));
+console.log(armstrongNumber(111));
