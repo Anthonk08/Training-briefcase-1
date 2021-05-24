722981357351b7007768f481926905c26d94b18f
Anthonk08 <anthonytineocabreja3@gmail.com>
2021-05-24 04:56:03 -0400

    Exercise #5 finished

--- Basic training/Javascript ES6/Practice/Exercises #1/5_Balanced_brackets.js
+++ Basic training/Javascript ES6/Practice/Exercises #1/5_Balanced_brackets.js
@@ -3,3 +3,31 @@ Balanced Brackets.
 Given a string possibly containing three types of braces ({}, [], ()), write a function that returns a Boolean
 indicating whether the given string contains a valid nesting of braces.
 */
+
+const balancedBrackets = str => {
+    if(str.length % 2 != 0){
+        return false;
+    }
+
+    const parentesis = {
+        '(':')',
+        '[':']',
+        '{':'}'
+    }
+    let grupoParentesis = [];
+    for(let letra of str) {
+        if(parentesis[letra]){
+            grupoParentesis.push(parentesis[letra]);
+            console.log(grupoParentesis);
+        }else {
+            if(grupoParentesis.pop() !== letra) {
+                return false;
+            }
+        }
+    }
+    return (!grupoParentesis.length)
+}
+
+console.log(balancedBrackets("({[]})")); //true
+//console.log(balancedBrackets("({[{))]})")); //false
+//console.log(balancedBrackets("({[{}[()]]})")); //true
