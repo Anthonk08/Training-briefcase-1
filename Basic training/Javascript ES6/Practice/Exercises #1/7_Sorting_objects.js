4bc0ade05964f8e5c0d05e34d62ccf7d4ed2d309
Anthonk08 <anthonytineocabreja3@gmail.com>
2021-05-24 04:06:03 -0400

    Exercise #7 finished

--- Basic training/Javascript ES6/Practice/Exercises #1/7_Sorting_objects.js
+++ Basic training/Javascript ES6/Practice/Exercises #1/7_Sorting_objects.js
@@ -1,4 +1,19 @@
 /*
 Sorting Objects.
 Given an array of objects, sort the objects by population size. Return the entire object.
+
+sort()  ordena un array según la función comparativa que se aplique,
+y si no se pasa una función comparativa, se ordena segun su posición del valor
+del string de acuerdo a su valor Unicode.
+}
 */
+
+const sortingObjects = numbers => {
+    numbers.sort((numA, numB) => {
+        return numA - numB;
+    });
+    return numbers;
+}
+
+console.log(sortingObjects([1, 11, 27, 2, 34, 123]));
+console.log(sortingObjects([-1, -11, -27, -2, -34, -123]));
