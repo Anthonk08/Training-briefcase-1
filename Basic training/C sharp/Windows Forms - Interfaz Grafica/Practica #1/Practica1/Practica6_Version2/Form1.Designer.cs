23864cf405b495415e00a5168fbba323e40ac0be
Anthonk08 <anthonytineocabreja3@gmail.com>
2021-11-26 15:47:57 -0400

    Add uses of MessageBox

--- Basic training/C sharp/Windows Forms - Interfaz Grafica/Practica #1/Practica1/Practica6_Version2/Form1.Designer.cs
+++ Basic training/C sharp/Windows Forms - Interfaz Grafica/Practica #1/Practica1/Practica6_Version2/Form1.Designer.cs
@@ -29,13 +29,60 @@ namespace Practica6_Version2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(27, 31);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(50, 20);
+            this.lblMensaje.TabIndex = 0;
+            this.lblMensaje.Text = "label1";
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(116, 31);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(94, 29);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(116, 87);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(94, 29);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(251, 222);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- Basic training/C sharp/Windows Forms - Interfaz Grafica/Practica #1/Practica1/Practica6_Version2/Form1.cs
+++ Basic training/C sharp/Windows Forms - Interfaz Grafica/Practica #1/Practica1/Practica6_Version2/Form1.cs
@@ -17,5 +17,33 @@ namespace Practica6_Version2
             InitializeComponent();
         }
 
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            //Mensaje normal
+            //MessageBox.Show("Hola a todos!!");
+
+            //Mensaje normal y el titulo de la caja
+            //MessageBox.Show("Hola a todos", "Mi MessageBox Anthony");
+
+            //Mensaje normal, el titulo de la caja, botones con una opcion como resultado y iconos
+            DialogResult result = MessageBox.Show("Te gusta el refresco?", "Preguntas rapidas", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Abort)
+            {
+                lblMensaje.Text = "Abortar";
+            }
+            else if (result == DialogResult.Retry)
+            {
+                lblMensaje.Text = "Reintentar";
+            }
+            else {
+                lblMensaje.Text = "Ignorar";
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
