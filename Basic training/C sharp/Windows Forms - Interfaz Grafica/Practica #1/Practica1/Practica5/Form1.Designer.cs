4dc8b6aa88a7a82e041b5ba3c4805a997cbab498
Anthonk08 <anthonytineocabreja3@gmail.com>
2021-11-26 15:07:05 -0400

    Add Practice 5 and Practica6_Versio2

---
+++ Basic training/C sharp/Windows Forms - Interfaz Grafica/Practica #1/Practica1/Practica5/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace Practica5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.gbOperaciones = new System.Windows.Forms.GroupBox();
+            this.rbDividir = new System.Windows.Forms.RadioButton();
+            this.rbMultiplicar = new System.Windows.Forms.RadioButton();
+            this.rbResta = new System.Windows.Forms.RadioButton();
+            this.rbSuma = new System.Windows.Forms.RadioButton();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.gbOperaciones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(40, 35);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(125, 27);
+            this.txtNumero1.TabIndex = 0;
+            this.txtNumero1.Text = "0";
+            this.txtNumero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(40, 98);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(125, 27);
+            this.txtNumero2.TabIndex = 1;
+            this.txtNumero2.Text = "0";
+            this.txtNumero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(200, 32);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(94, 29);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular\r\n";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(200, 98);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(94, 29);
+            this.btnCerrar.TabIndex = 3;
+            this.btnCerrar.Text = "Cerrar\r\n";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // gbOperaciones
+            // 
+            this.gbOperaciones.Controls.Add(this.rbDividir);
+            this.gbOperaciones.Controls.Add(this.rbMultiplicar);
+            this.gbOperaciones.Controls.Add(this.rbResta);
+            this.gbOperaciones.Controls.Add(this.rbSuma);
+            this.gbOperaciones.Location = new System.Drawing.Point(40, 198);
+            this.gbOperaciones.Name = "gbOperaciones";
+            this.gbOperaciones.Size = new System.Drawing.Size(254, 107);
+            this.gbOperaciones.TabIndex = 4;
+            this.gbOperaciones.TabStop = false;
+            this.gbOperaciones.Text = "Operaciones";
+            // 
+            // rbDividir
+            // 
+            this.rbDividir.AutoSize = true;
+            this.rbDividir.Location = new System.Drawing.Point(131, 67);
+            this.rbDividir.Name = "rbDividir";
+            this.rbDividir.Size = new System.Drawing.Size(74, 24);
+            this.rbDividir.TabIndex = 3;
+            this.rbDividir.Text = "Dividir";
+            this.rbDividir.UseVisualStyleBackColor = true;
+            // 
+            // rbMultiplicar
+            // 
+            this.rbMultiplicar.AutoSize = true;
+            this.rbMultiplicar.Location = new System.Drawing.Point(131, 26);
+            this.rbMultiplicar.Name = "rbMultiplicar";
+            this.rbMultiplicar.Size = new System.Drawing.Size(101, 24);
+            this.rbMultiplicar.TabIndex = 2;
+            this.rbMultiplicar.Text = "Multiplicar";
+            this.rbMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // rbResta
+            // 
+            this.rbResta.AutoSize = true;
+            this.rbResta.Location = new System.Drawing.Point(16, 66);
+            this.rbResta.Name = "rbResta";
+            this.rbResta.Size = new System.Drawing.Size(66, 24);
+            this.rbResta.TabIndex = 1;
+            this.rbResta.Text = "Resta";
+            this.rbResta.UseVisualStyleBackColor = true;
+            // 
+            // rbSuma
+            // 
+            this.rbSuma.AutoSize = true;
+            this.rbSuma.Checked = true;
+            this.rbSuma.Location = new System.Drawing.Point(17, 26);
+            this.rbSuma.Name = "rbSuma";
+            this.rbSuma.Size = new System.Drawing.Size(67, 24);
+            this.rbSuma.TabIndex = 0;
+            this.rbSuma.TabStop = true;
+            this.rbSuma.Text = "Suma";
+            this.rbSuma.UseVisualStyleBackColor = true;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(40, 148);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(98, 28);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(332, 328);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.gbOperaciones);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.gbOperaciones.ResumeLayout(false);
+            this.gbOperaciones.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.GroupBox gbOperaciones;
+        private System.Windows.Forms.RadioButton rbDividir;
+        private System.Windows.Forms.RadioButton rbMultiplicar;
+        private System.Windows.Forms.RadioButton rbResta;
+        private System.Windows.Forms.RadioButton rbSuma;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
