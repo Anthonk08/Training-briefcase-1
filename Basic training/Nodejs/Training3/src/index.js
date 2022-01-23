1818b9554b9465942fc94d777257edfedf1e7f38
Anthonk08 <anthonytineocabreja3@gmail.com>
2022-01-22 20:11:22 -0400

    Aprendiendo sobre rutas y request

--- Basic training/Nodejs/Training3/src/index.js
+++ Basic training/Nodejs/Training3/src/index.js
@@ -1,22 +1,57 @@
 // Llamamos a express
+const { urlencoded } = require('express');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
-// Llamamos una aplicacion ejecutando express
+// creamos aplicacion para ejecutando express
 const app = express();
 
-// Para agregar fecha de hoy, tonteria mia
-const today = new Date();
-const todayDate = `${today.getFullYear()} - ${today.getMonth() + 1} - ${today.getDate()}`; // format sql YYYY - MM - DD
+//Middleware
+//Para obtener la informacion envia por el postman
+app.use(urlencoded());
+// Nos permite resivir informacion y arreglarla de forma que se pueda leer en los paramentros
+app.use(express.json());
+//
+app.use(cookieParser());
 
-//Registrar un get request
+//Registrar un GET request
 app.get('/users/:userId', (req, res) => {
-// Agregamos la ruta express de lo que queremos obtener, esta funcion resive varios argumentos entre estos request y response: req y res
+// Agregamos la ruta express de lo que queremos obtener, esta funcion recibe varios argumentos entre estos request y response: req y res
     res.json({
         id: req.params.userId,
         name: 'Anthony',
         apellidos: 'Tineo Cabreja',
         age: 26,
         createDay: todayDate,
+    });
+});
+
+//Registrar un POST request
+app.post('/users/:userId', (req, res) => {
+//  Cualquier parametro que este dentro de userId sera accesado por el req.params
+    const { userId } = req.params || {};
+    const { name, apellidos, age, test } = req.query || {};
+    const { test_cookie } = req.cookies;
+    //const { name, apellidos, age } = req.body || {}; // No funciono el jodio body
+
+    // Headers
+    const contentType = req.headers['content-type'];
+
+    //cookie
+    //const cookie
+
+    // Enviar la ip
+    const ip = app.ip;
+
+// Agregamos la ruta express de lo que queremos obtener, esta funcion recibe varios argumentos entre estos request y response: req y res
+    res.json({
+        id: userId,
+        name,
+        apellidos,
+        age,
+        test,
+        contentType,
+        test_cookie,
     });
 });
 
