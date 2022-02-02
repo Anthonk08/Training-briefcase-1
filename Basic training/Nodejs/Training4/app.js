bf6285120bc6f7da3d751a74664c71bc492efbe4
Anthonk08 <anthonytineocabreja3@gmail.com>
2022-02-02 17:20:36 -0400

    Path module learning

--- Basic training/Nodejs/Training4/app.js
+++ Basic training/Nodejs/Training4/app.js
@@ -1,5 +1,6 @@
-function sayHello(name) {
-    console.log(`hello ${name}`);
-}
+//El pathmódulo proporciona utilidades para trabajar con rutas de archivos y directorios
+const path = require('path');
 
-//sayHello('Anthony');
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
