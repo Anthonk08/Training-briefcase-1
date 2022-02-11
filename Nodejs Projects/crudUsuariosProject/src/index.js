bd1f441fde34a55435b5aeab0d059a5dd8ebc62c
Anthonk08 <anthonytineocabreja3@gmail.com>
2022-02-11 12:29:31 -0400

    Integrate TravisCI

--- Nodejs Projects/crudUsuariosProject/src/index.js
+++ Nodejs Projects/crudUsuariosProject/src/index.js
@@ -1,2 +1,10 @@
-console.log('Hola');
-console.log('Anthony Tineo');
+const express = require('express');
+// const router = require('../routes/usuarios');
+
+// Middleware
+const app = express();
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`El puerto ${port}, inicio correctamente!`);
+});
