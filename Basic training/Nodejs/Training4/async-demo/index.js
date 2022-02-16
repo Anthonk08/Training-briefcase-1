ea0965c2450ab4c4dca6a7664f2f985326ccdf49
Anthonk08 <anthonytineocabreja3@gmail.com>
2022-02-16 18:59:20 -0400

    Async js

--- Basic training/Nodejs/Training4/async-demo/index.js
+++ Basic training/Nodejs/Training4/async-demo/index.js
@@ -1,45 +1,64 @@
-// Asynchronous
-console.log(`Before`);
-getUser(1, user => {
-    getRepositories(user.gitHubUsername, (repo) => {
-        getCommits(repo, (commits) => {
-            // CALLBACKS HELL
-
-        });
-    });
-});
-console.log(`After`);
-
-// Synchronous
 console.log('Before');
-const user = getUser(1);
-const repos = getRepositories(user.gitHubUsername);
-const commits = getCommits(repos[0]);
+
+// Callback-based approach
+//
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUsername, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     })
+//   })
+// });
+
+// Promise-based approach
+/*
+getUser(1)
+  .then(user => getRepositories(user.gitHubUsername))
+  .then(repos => getCommits(repos[0]))
+  .then(commits => console.log('Commits', commits))
+  .catch(err => console.log('Error', err.message));
+*/
+// Async and Await approach
+async function displayCommits() {
+  try{
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }
+  catch(err) {
+    console.log('Error', err.message);
+  }
+}
+displayCommits();
+
 console.log('After');
 
-// Callbacks
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log("Escribiendo un usuario en la base de datos");
-        callback({id: id, gitHubUsername: 'Anthony Tineo'});
-        }, 2000);
-}
-
-// Exercise
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 3000);
-}
-
-// Promises
-// Async/await
-
-/*
 function getUser(id) {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work 
     setTimeout(() => {
-        console.log("Escribiendo un usuario en la base de datos");
-        return {id: id, gitHubUsername: 'Anthony Tineo'};
-        }, 2000);
+      console.log('Reading a user from a database...');
+      resolve({ id: id, gitHubUsername: 'mosh' });
+    }, 2000);
+  });
 }
-*/
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      // resolve(['repo1', 'repo2', 'repo3']);
+      reject(new Error('No se puedo obtener el repo'));
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['commit']);
+    }, 2000);
+  });
+}
