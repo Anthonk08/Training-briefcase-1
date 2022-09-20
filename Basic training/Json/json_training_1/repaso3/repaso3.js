d40779d31ff50b1214dd3101a6d8f69dfcc96d3c
Anthonk08 <anthonytineocabreja@gmail.com>
2022-09-20 17:16:36 -0400

    Getter and Setter used

--- Basic training/Json/json_training_1/repaso3/repaso3.js
+++ Basic training/Json/json_training_1/repaso3/repaso3.js
@@ -7,8 +7,17 @@ const yo = {
 }
 
 let yo_string = JSON.stringify(yo);
+// setItem, permite almacenar valores en el localstorage
+// ("Clave", objeto)
+localStorage.setItem("Objeto_Anthony", yo_string);
+
+// getItem es utilizado para buscar un valor en el localstorage
+// ("clave")
+
+let localstorage_date = localStorage.getItem("Objeto_Anthony");
 
 let yo_parse = JSON.parse(yo_string)
 
 alert(yo_string);
 alert(yo_parse);
+alert(localstorage_date);
